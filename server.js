161894e22641359f334891054795f688ca7f05d5
Devam M <devam493@gmail.com>
2019-09-03 10:24:22 -0400

    added json middleware for express

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 
 const app = express();
 const port = 8080;
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/', require('./routes.js'));
 
 app.listen(port, () => {
