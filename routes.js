1c924a8b2d0894dd2c23c268646ff4f1c742299c
Devam M <devam493@gmail.com>
2019-09-03 10:22:53 -0400

    created routes for spotify oauth and callback success page

---
+++ routes.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+const {SPOTIFY_CLIENT, CALLBACK_URL} = require('./secrets.js');
+
+
+router.get('/login', (req, res) => {
+    const scopes = 'playlist-read-collaborative playlist-modify-public';
+    res.redirect('https://accounts.spotify.com/authorize' +
+        '?response_type=code' +
+        '&client_id=' + SPOTIFY_CLIENT +
+        (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+        '&redirect_uri=' + encodeURIComponent(CALLBACK_URL)
+    );
+})
+
+router.get("/success", (req, res) => {
+    res.send('success!');
+})
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+app.use('/', require('./routes.js'));
 
 app.listen(port, () => {
     console.log(`spotify test server listening on port ${port}!`)
