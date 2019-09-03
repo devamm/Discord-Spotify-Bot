b1e1713305a595a2741d823ac9a1e3ab69e0cfd0
Devam M <devam493@gmail.com>
2019-09-03 17:39:57 -0400

    moved auth url to secrets

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const { OW_TOKEN, DEV_ID, PLAYLIST_ID, CHANNEL_ID } = require("./secrets.js");
+const { OW_TOKEN, DEV_ID, PLAYLIST_ID, CHANNEL_ID, AUTH_URL } = require("./secrets.js");
 const { getAuthCode, getInitialToken, getRefreshedToken} = require("./auth.js");
 const axios = require("axios");
 
@@ -33,7 +33,7 @@ const listener = async (msg, client) => {
     }
     
     if (msg.content == "!auth" && msg.author.id == DEV_ID) {
-        await msg.channel.send("Please use this link to connect me to Spotify!\nhttp://localhost:8080/auth");
+        await msg.channel.send("Please use this link to connect me to Spotify!\n"+AUTH_URL);
         try {
             const code = await getAuthCode();
             const tokens = await getInitialToken(code);
@@ -52,35 +52,46 @@ const listener = async (msg, client) => {
 };
 
 const addToPlaylist = async (song_id, msg) => {
-    try {
-        if (validToken) {
-            //await msg.channel.send(`adding song id "${song_id}" to playlist`)
-            const url = `https://api.spotify.com/v1/playlists/${PLAYLIST_ID}/tracks`;
-
-            const { data: result } = await axios.post(
-                url, {
-                    uris: [`spotify:track:${song_id}`]
-                }, {
-                    headers: { Authorization: "Bearer " + ACCESS_TOKEN }
-                }
-            );
-        } else {
-            //token expired, revalidate token
-            console.log("refreshing token");
-            const newToken = await getRefreshedToken(REFRESH_TOKEN);
-            if (newToken == null) {
-                await msg.channel.send("something went wrong generating new token");
+    let attemptedRetry = false;
+    while(true){
+        try {
+            if (validToken) {
+                //await msg.channel.send(`adding song id "${song_id}" to playlist`)
+                const url = `https://api.spotify.com/v1/playlists/${PLAYLIST_ID}/tracks`;
+    
+                const { data: result } = await axios.post(
+                    url, {
+                        uris: [`spotify:track:${song_id}`]
+                    }, {
+                        headers: { Authorization: "Bearer " + ACCESS_TOKEN }
+                    }
+                );
                 return;
+            } else {
+                //token expired, revalidate token
+                if(attemptedRetry){
+                    await msg.channel.send('API Error');
+                    return;
+                }
+                attemptedRetry = true;
+                console.log("refreshing token");
+                const newToken = await getRefreshedToken(REFRESH_TOKEN);
+                if (newToken == null) {
+                    await msg.channel.send("something went wrong generating new token");
+                    return;
+                }
+                ACCESS_TOKEN = newToken.access_token;
+                REFRESH_TOKEN = newToken.refresh_token ? newToken.refresh_token : REFRESH_TOKEN;
+                const expiry = newToken.expires_in;
+                validToken = true;
+                
+                invalidateToken(expiry);
             }
-            ACCESS_TOKEN = newToken.access_token;
-            REFRESH_TOKEN = newToken.refresh_token ? newToken.refresh_token : REFRESH_TOKEN;
-            const expiry = newToken.expires_in;
-            
-            invalidateToken(expiry);
+        } catch (e) {
+            await msg.channel.send("something went wrong");
+            console.log(e);
+            return;
         }
-    } catch (e) {
-        await msg.channel.send("something went wrong");
-        console.log(e);
     }
 };
 
