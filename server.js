74ff138e1eb27e62497b9b9dde36a60152774ddc
Devam M <devam493@gmail.com>
2019-09-05 16:17:50 -0400

    added route to catch all web requests to prevent application crashes

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', require('./routes.js'));
 
+app.get("*", (req, res) => {
+    res.send("I am a bot. Hello!");
+})
+
 app.listen(PORT, () => {
     console.log(`spotify test server listening on port ${PORT}!`)
 })
